407ab2f2a0535e13007e21ae40b3bc2fdac6fc04
Mario Peixoto <mario.peixoto@gmail.com>
2011-08-26 20:41:32 -0300

    Added user confirmation to sign up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
          
   attr_accessor :login
