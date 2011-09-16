3e069ccf867aa51885005c5037a74e80212a4a8e
Mario Peixoto <mario.peixoto@gmail.com>
2011-09-16 17:03:50 -0300

    Setting locale based on parameter passed too.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :set_locale
+  
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = t "need.login"
     redirect_to new_user_session_path
+  end
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
